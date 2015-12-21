c8ee30ebbb516463350e03369280a5d224d1d86a
Martino Facchin <m.facchin@arduino.cc>
2015-12-21 16:00:22 +0100

    HRMExample: fix using char[2] characteristic

--- Arduino101HeartRateMonitor/Arduino101HeartRateMonitor.ino
+++ Arduino101HeartRateMonitor/Arduino101HeartRateMonitor.ino
@@ -27,9 +27,10 @@ BLEPeripheral blePeripheral;       // BLE Peripheral Device (the board you're pr
 BLEService heartRateService("180D"); // BLE Heart Rate Service
 
 // BLE Heart Rate Measurement Characteristic"
-BLEUnsignedCharCharacteristic heartRateChar("2A37",  // standard 16-bit characteristic UUID
-    BLERead | BLENotify);     // remote clients will be able to
-                              // get notifications if this characteristic changes
+BLECharacteristic heartRateChar("2A37",  // standard 16-bit characteristic UUID
+    BLERead | BLENotify, 2);  // remote clients will be able to get notifications if this characteristic changes
+                              // the characteristic is 2 bytes long as the first field needs to be "Flags" as per BLE specifications
+                              // https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
 
 int oldHeartRate = 0;  // last heart rate reading from analog input
 long previousMillis = 0;  // last time the heart rate was checked, in ms
@@ -46,7 +47,6 @@ void setup() {
   blePeripheral.setAdvertisedServiceUuid(heartRateService.uuid());  // add the service UUID
   blePeripheral.addAttribute(heartRateService);   // Add the BLE Heart Rate service
   blePeripheral.addAttribute(heartRateChar); // add the Heart Rate Measurement characteristic
-  heartRateChar.setValue(oldHeartRate);   // initial value for this characteristic
 
   /* Now activate the BLE device.  It will start continuously transmitting BLE
      advertising packets and will be visible to remote BLE central devices
@@ -93,7 +93,8 @@ void updateHeartRate() {
   if (heartRate != oldHeartRate) {      // if the heart rate has changed
     Serial.print("Heart Rate is now: "); // print it
     Serial.println(heartRate);
-    heartRateChar.setValue(heartRate);  // and update the heart rate measurement characteristic
+    const unsigned char heartRateCharArray[2] = { 0, (char)heartRate };
+    heartRateChar.setValue(heartRateCharArray, 2);  // and update the heart rate measurement characteristic
     oldHeartRate = heartRate;           // save the level for next comparison
   }
 }
