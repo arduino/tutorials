b042021bd26aa6449d41452d30711a9d1603fdec
agdl <a.guadalupi@arduino.cc>
2016-10-06 11:14:55 +0200

    Modified example to use WiFi.getTime();

--- MKR1000WiFiRTC/MKR1000WiFiRTC.ino
+++ MKR1000WiFiRTC/MKR1000WiFiRTC.ino
@@ -6,6 +6,8 @@
   created 08 Jan 2016
   by Arturo Guadalupi <a.guadalupi@arduino.cc>
 
+  modified 06 Oct 2016
+
   http://arduino.cc/en/Tutorial/WiFiRTC
   This code is in the public domain.
 */
@@ -17,20 +19,13 @@
 
 RTCZero rtc;
 
-char ssid[] = "yourNetwork";     //  your network SSID (name)
-char pass[] = "yourPassword";   // your network password
-int keyIndex = 0;            // your network key Index number (needed only for WEP)
+char ssid[] = "yourNetwork";	//  your network SSID (name)
+char pass[] = "yourPassword";	// your network password
+int keyIndex = 0;							// your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 
-// Used for NTP
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
-
-const int GMT = 1; //change this to adapt it to your time zone
+const int GMT = 2; //change this to adapt it to your time zone
 
 void setup() {
   Serial.begin(115200);
@@ -61,7 +56,7 @@ void setup() {
   unsigned long epoch;
   int numberOfTries = 0, maxTries = 6;
   do {
-    epoch = readLinuxEpochUsingNTP();
+    epoch = WiFi.getTime();
     numberOfTries++;
   }
   while ((epoch == 0) || (numberOfTries > maxTries));
@@ -86,42 +81,6 @@ void loop() {
   delay(1000);
 }
 
-unsigned long readLinuxEpochUsingNTP()
-{
-  Udp.begin(localPort);
-  sendNTPpacket(timeServer); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  delay(1000);
-
-  if ( Udp.parsePacket() ) {
-    Serial.println("NTP time received");
-    // We've received a packet, read the data from it
-    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-
-    // now convert NTP time into everyday time:
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-
-    Udp.stop();
-    return (secsSince1900 - seventyYears);
-  }
-
-  else {
-    Udp.stop();
-    return 0;
-  }
-}
-
 void printTime()
 {
   print2digits(rtc.getHours() + GMT);
@@ -143,30 +102,6 @@ void printDate()
   Serial.print(" ");
 }
 
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress & address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
-}
 
 void printWifiStatus() {
   // print the SSID of the network you're attached to:
