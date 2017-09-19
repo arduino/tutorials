28f286ac035943210260ff11bfe384444f1e2d41
marcus johansson <mj.emeth@gmail.com>
2017-09-19 08:16:04 +0200

    Update MKR1000ScheduledWiFiSSLWebClient.ino

--- MKR1000ScheduledWiFiSSLWebClient/MKR1000ScheduledWiFiSSLWebClient.ino
+++ MKR1000ScheduledWiFiSSLWebClient/MKR1000ScheduledWiFiSSLWebClient.ino
@@ -15,7 +15,7 @@
 #include <WiFi101.h>
 #include <RTCZero.h>
 
-#include "arduino_secrets.h" #include "arduino_secrets.h" 
+#include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
